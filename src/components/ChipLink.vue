e2d3c3766f79d3bd14ffb895d6b55ab5b400cb9a
David Rios <drios9107@gmail.com>
2026-05-04 21:00:52 -0400

    created ChipLink component

---
+++ src/components/ChipLink.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps({
+    href: String,
+    title: String,
+    color:String,
+    icon:String
+})
+</script>
+<template>
+    <a :href="props.href" target="_blank" class="no-margin">
+        <q-chip :color="props.color" text-color="white" :icon="props.icon" class="chip">
+            {{ props.title }}
+        </q-chip>
+    </a>
+</template>
