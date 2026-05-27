b7fd19d7ca9a0d7ccf0c0dc7fbdf55bbbebd4bd8
David Rios <drios9107@gmail.com>
2026-05-27 12:17:11 -0400

    added profile link styles

--- src/components/ChipLink.vue
+++ src/components/ChipLink.vue
@@ -2,8 +2,8 @@
 const props = defineProps({
     href: String,
     title: String,
-    color:String,
-    icon:String
+    color: String,
+    icon: String
 })
 </script>
 <template>
@@ -19,5 +19,10 @@ const props = defineProps({
     user-select: none;
     height: 28px;
     margin: 8px 4px 4px 8px;
+    transition: transform 0.15s ease;
+}
+
+.chip:hover {
+    transform: translateY(-2px);
 }
 </style>
